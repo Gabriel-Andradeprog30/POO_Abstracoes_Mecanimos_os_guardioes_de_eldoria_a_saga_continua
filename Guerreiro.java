590e4b88c19f17abf41786bd9122d41783de4f36
Gabriel-Andradeprog30 <157922939+Gabriel-Andradeprog30@users.noreply.github.com>
2026-05-11 21:40:52 -0300

    Update Guerreiro.java

--- Guerreiro.java
+++ Guerreiro.java
@@ -1,20 +1,36 @@
 package eldoria;
-
-/**
- * Classe que representa um Guerreiro no reino de Eldoria.
- * Herda de Personagem e implementa habilidades específicas de combate.
- */
 public class Guerreiro extends Personagem {
+    private double forca;
+    
+    public Guerreiro(String nome, int nivel, int pontosDeVida, double poderBase, double forca) {
+        super(nome, "Guerreiro", nivel, pontosDeVida, poderBase);
+        this.forca = forca;
+    }
 
-     // * Construtor para criar um Guerreiro.
+    public double getForca() {
+        return forca;
+    }
 
-    /**
-     * Sobrescrita do método usarHabilidade() específica para Guerreiros.
-     * Demonstra polimorfismo e vinculação dinâmica.
-     */
+    public void setForca(double forca) {
+        this.forca = forca;
+    }
+
+
+    @Override
+    void exibirStatus() {
+        System.out.println("Nome: " + getNome());
+        System.out.println("Classe: " + getClasse());
+        System.out.println("Nível: " + getNivel());
+        System.out.println("Pontos de Vida: " + getPontosDeVida());
+        System.out.println("Poder Base: " + getPoderBase());
+        System.out.println("Forca: " + getForca());
+        
+    } 
+
     @Override
     public void usarHabilidade() {
+        System.out.println("Habilidade: " + getNome() + " Ataque Espada Flamejante!");
 
     }
-}
 
+}
