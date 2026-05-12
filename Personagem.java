6ec4d9d657139853fad93881b7ece5ea6bddbf81
Gabriel-Andradeprog30 <157922939+Gabriel-Andradeprog30@users.noreply.github.com>
2026-05-11 21:52:09 -0300

    Update Personagem.java

--- Personagem.java
+++ Personagem.java
@@ -8,14 +8,67 @@ import java.util.Objects;
  */
 public class abstract Personagem {
     // Atributos privados (encapsulamento)
-
+    private String nome;
+    private String classe;
+    private int nivel;
+    private int pontosDeVida;
+    private double poderBase;
 
     // * Construtor que inicializa todos os atributos do personagem.
-
+    public Personagem(String nome, String classe, int nivel, int pontosDeVida, double poderBase) {
+        this.nome = nome;
+        this.classe = classe;
+        this.nivel = nivel;
+        this.pontosDeVida = pontosDeVida;
+        this.poderBase = poderBase;
     // Getters
+     public String getNome() {
+        return nome;
+    }
 
-    // Setters com validações
+    public String getClasse() {
+        return classe;
+    }
 
+    public int getNivel() {
+        return nivel;
+    }
+
+    public int getPontosDeVida() {
+        return pontosDeVida;
+    }
+
+    public double getPoderBase() {
+        return poderBase;
+    }
+
+    // Setters com validações 
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setClasse(String classe) {
+        this.classe = classe;
+    }
+
+
+    public void setNivel(int nivel) {
+        if (nivel > 0) {
+            this.nivel = nivel;
+        }
+    }
+
+    public void setPontosDeVida(int pontosDeVida) {
+        if (pontosDeVida >= 0) {
+            this.pontosDeVida = pontosDeVida;
+        }
+    }
+
+    public void setPoderBase(double poderBase) {
+        if (poderBase >= 0) {
+            this.poderBase = poderBase;
+        }
+    }
     // * Criar métodos que define os pontos de vida do personagem.
 
 
